4131e5200317ea0a49894560fe3b01585a2add4a
Tom Hudson <trhudson@gmail.com>
2020-08-28 16:36:14 -0400

    created about page and routes

--- app.js
+++ app.js
@@ -19,8 +19,13 @@ app.get("/", function(req, res) {
   res.render("index");
 });
 
+app.get("/index", function(req, res) {
+  res.render("index");
+});
 
-
+app.get("/about", function (req, res) {
+  res.render("about");
+});
 
 
 
