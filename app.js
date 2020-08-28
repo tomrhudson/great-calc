5adbcf0a76c5a951b632b9d75af68e3f63f1e228
Tom Hudson <trhudson@gmail.com>
2020-08-28 08:36:17 -0400

    setup basic file formats
    added dependencies, and basic files to get started on project

---
+++ app.js
@@ -0,0 +1,25 @@
+//jshint esversion:6
+require('dotenv').config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res){
+  res.render("index");
+});
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000.");
+});
